056a37b9555ee11b7db4ff356b0fbb6feb0970b1
POWER <clay.degruchy@gmail.com>
2021-05-25 22:21:55 +0200

    improved transofrm

--- server.py
+++ server.py
@@ -16,6 +16,22 @@ def make_relative(locations):
         locations[location][0] = locations[location][0] - locations["bottom"][0]
         locations[location][1] = locations[location][1] - locations["bottom"][1]
 
+# find the modifier for x and y, this should give us a 0-1 range of where a point can be
+    top_0_factor = 1/locations["top"][0]
+    top_1_factor = 1/locations["top"][1]
+
+# set the maxes to 1
+    locations["top"][0] = 1
+    locations["top"][1] = 1
+
+    for location in [x for x in locations if x != "bottom" and x!= "top"]:
+        locations[location][0] = locations[location][0] * top_0_factor
+        locations[location][1] = locations[location][1] * top_1_factor
+
+
+    # for location in locations:
+    #     locations[location][0] =
+
     return locations
 
 
